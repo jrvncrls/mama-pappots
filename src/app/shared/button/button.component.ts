d80365eb6e3f8e119879f1374abb972f4a70a111
Jervin Carlos <jlcarloswork@gmail.com>
2025-05-02 23:05:40 +0800

    Add shared button

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,15 @@
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [NgClass, NgStyle],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  label = input('Button');
+  color = input<'primary' | 'secondary'>('primary');
+  borderRadius = input<10 | 30>(10);
+  clickChange = output();
+}
