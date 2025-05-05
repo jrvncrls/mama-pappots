1e247ed8ad3e5f4a8e7657b62e16f542f1afc9a6
Jervin Carlos <jlcarloswork@gmail.com>
2025-05-05 22:03:39 +0800

    Add footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'products', component: ProductsComponent },
-];
+export const routes: Routes = [{ path: '', component: HomeComponent }];
